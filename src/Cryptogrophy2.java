5a3e48ed420e16c47a44187de3eecacc877e9807
ahance21 <42975655+ahance21@users.noreply.github.com>
2019-05-06 12:42:13 -0400

    Add files via upload

---
+++ src/Cryptogrophy2.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Cryptogrophy2
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Would you like to encrypt or decrypt?");
+		
+	    
+		
+		
+
+	}
+	public static String caesar_cipher(String fileName, boolean encrypt, int shiftAmount) throws IOException
+	{
+		if (encrypt == true)
+		{
+			
+		}
+	}
+	
+
+}
